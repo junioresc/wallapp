4ef39bcfb674c48ccec21a63fdd352751acafcd6
Junior Escobar <juniorescobar@Juniors-MacBook-Pro-2.local>
2022-07-30 19:38:43 -0400

    added mongoose to backend and created authentication logic and started work on the header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    return(
+        <header>
+            <div>
+                <Link to='/'>
+                    <h1>Wall App</h1>
+                </Link>
+
+                <nav>
+                    
+                </nav>
+            </div>
+        </header>
+    )
+}
