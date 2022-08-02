40f30bb9109429bd83f541f408970c1893380c04
junioresc <junioresc1092@gmail.com>
2022-08-01 23:32:38 -0400

    created the PostForm component

--- client/src/components/PostForm/index.js
+++ client/src/components/PostForm/index.js
@@ -1,3 +1,94 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import { useMutation } from '@apollo/client';
 import { ADD_POST } from '../../utils/mutations';
+import { QUERY_POSTS, QUERY_ME } from '../../utils/queries';
+
+const PostForm = () => {
+    const [postText, setPostText] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const [addPost, { error }] = useMutation(ADD_POST, {
+        update(cache, { data: {addPost } }) {
+            try {
+                const { posts } = cache.readQuery({ query: QUERY_POSTS});
+
+                cache.writeQuery({
+                    query: QUERY_POSTS,
+                    data: { posts: [addPost, ...posts] }
+                });
+            } catch (e) {
+                console.error(e);
+                setShowAlert(true);
+            }
+
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, posts: [...me.posts, addPost] } }
+            });
+        }
+    });
+
+    const handleChange = event => {
+        if(event.target.value.length <= 1000) {
+            setPostText(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValiditiy() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        try {
+            await addPost({
+                variables: { postText }
+            });
+
+            setPostText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+            setShowAlert(true)
+        }
+    };
+
+    return(
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+                Something went wrong with your post! :(
+            </Alert>
+            <Form.Group controlId='postText'>
+                <Form.Label htmlFor='characterCount'>Character Count: { characterCount }/1000</Form.Label>
+                <Form.Control
+                    type='text'
+                    as='textarea'
+                    placeholder='Add a new Post? Share your thoughts and opinions...'
+                    name='postText'
+                    value={postText}
+                    onChange={handleChange}
+                    required
+                    />
+                    <Form.Control.Feedback type='invalid'>Your post can't be empty!</Form.Control.Feedback>
+            </Form.Group>
+            <Button
+                disabled={!(postText)}
+                type='submit'
+                variant='outline-primary'>
+                    Submit your Post
+                </Button>
+        </Form>
+    );
+};
+
+export default PostForm;
